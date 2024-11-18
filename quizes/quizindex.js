e7871e576cbca529b45e617a3fd73d1c0d8cee62
LinlyBoi <linlysmolworthy@gmail.com>
2024-11-18 11:32:12 +0200

    nlp regex quiz )

---
+++ quizes/quizindex.js
@@ -0,0 +1,7 @@
+var QuizTitles = [
+  ['Regular Expressions', 'quiz2.html']
+];
+// construct the drop down menu if QuizTitles has some entries
+if (QuizTitles.length > 0 && quizindex_menu) {
+    create_quizindex_menu();
+}
---
+++ quizes/regex/wq-regex.js
@@ -0,0 +1,82 @@
+QuizSpecifications[1]=[];
+QuizSpecifications[1].type="input";
+QuizSpecifications[1].value="<table class="equation-star"><tr><td>
+<!--l. 8--><math 
+ xmlns="http://www.w3.org/1998/Math/MathML"  
+display="block" class="equation">
+                            <mi 
+>a</mi> <mo 
+class="MathClass-bin" stretchy="false">−</mo> <mi 
+>z</mi><mi 
+>A</mi> <mo 
+class="MathClass-bin" stretchy="false">−</mo> <mi 
+>Z</mi>
+</math></td></tr></table>
++";
+QuizSpecifications[1].comparison="string";
+QuizSpecifications[2]=[];
+QuizSpecifications[2].type="input";
+QuizSpecifications[2].value="<table class="equation-star"><tr><td>
+<!--l. 14--><math 
+ xmlns="http://www.w3.org/1998/Math/MathML"  
+display="block" class="equation">
+                               <mn>0</mn> <mo 
+class="MathClass-bin" stretchy="false">−</mo> <mn>9</mn>
+</math></td></tr></table>
+<table class="equation-star"><tr><td>
+                                                                  
+
+                                                                  
+<!--l. 14--><math 
+ xmlns="http://www.w3.org/1998/Math/MathML"  
+display="block" class="equation">
+                            <mi 
+>a</mi> <mo 
+class="MathClass-bin" stretchy="false">−</mo> <mi 
+>z</mi><mi 
+>A</mi> <mo 
+class="MathClass-bin" stretchy="false">−</mo> <mi 
+>Z</mi>
+</math></td></tr></table>
++";
+QuizSpecifications[2].comparison="string";
+QuizSpecifications[3]=[];
+QuizSpecifications[3].type="input";
+QuizSpecifications[3].value="∖b<table class="equation-star"><tr><td>
+<!--l. 20--><math 
+ xmlns="http://www.w3.org/1998/Math/MathML"  
+display="block" class="equation">
+                            <mi 
+>a</mi> <mo 
+class="MathClass-bin" stretchy="false">−</mo> <mi 
+>z</mi><mi 
+>A</mi> <mo 
+class="MathClass-bin" stretchy="false">−</mo> <mi 
+>Z</mi>
+</math></td></tr></table>
++";
+QuizSpecifications[3].comparison="string";
+QuizSpecifications[4]=[];
+QuizSpecifications[4].type="input";
+QuizSpecifications[4].value="∖b<table class="equation-star"><tr><td>
+                                                                  
+
+                                                                  
+<!--l. 26--><math 
+ xmlns="http://www.w3.org/1998/Math/MathML"  
+display="block" class="equation">
+                               <mi 
+>a</mi> <mo 
+class="MathClass-bin" stretchy="false">−</mo> <mi 
+>z</mi>
+</math></td></tr></table>
++e∖b";
+QuizSpecifications[4].comparison="string";
+QuizSpecifications[5]=[];
+QuizSpecifications[5].type="input";
+QuizSpecifications[5].value="∖w";
+QuizSpecifications[5].comparison="string";
+toggle_side_menu();
+shuffleQuestions();
+initSession();
+gotoQuestion(1);
