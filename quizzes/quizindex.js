58c3d679b294c8bc64a25ec985b1b474aeb6245a
LinlyBoi <linlysmolworthy@gmail.com>
2024-11-22 14:18:51 +0200

    )

--- quizzes/quizindex.js
+++ quizzes/quizindex.js
@@ -1,7 +1,8 @@
 var QuizTitles = [
   ['Regular Expressions', 'regex'],
   ['Tokenisation', 'tokenisation'],
-  ['NLP Tasks', 'nlp-tasks']
+  ['NLP Tasks', 'nlp-tasks'],
+  ['Word Embedding', 'word-embeddings']
 ];
 // construct the drop down menu if QuizTitles has some entries
 if (QuizTitles.length > 0 && quizindex_menu) {
